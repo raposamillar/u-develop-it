23bb8d731150f0a227336edef711dfa02352c107
raposamillar <lisa.raposamillar@gmail.com>
2022-09-20 14:13:22 -0400

    password changed

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Like2code!',
+    password: 'Newcode!',
     database: 'election'
   });
 
